67cf53ad26f88358a450e92a629684131dea8794
Kevin O'Keeffe <kevokeeffe@gmail.com>
2019-12-07 21:27:27 +0000

    Test restructuring and bug fix

--- cypress/integration/register-page.spec.js
+++ cypress/integration/register-page.spec.js
@@ -9,40 +9,6 @@ describe("Register page", () => {
             cy.contains('nav-link', 'Logout').click()
         }
     });
-    describe("Navigation bar", () => {
-        it("Shows a header", () => {
-            cy.get(".navbar")
-                .within(()=>{
-                    cy.get(".navbar-brand").should("contain", "Restaurant Manager")
-                        .eq(0)
-                    cy.get(".navbar-toggler")
-                        .eq(0)
-                });
-        });
-
-        it("Shows the required links", () => {
-            cy.get(".collapse")
-                .eq(0)
-                .within(() => {
-                    cy.get(".navbar-nav")
-                        .eq(0)
-                        .within(() => {
-                            cy.get(".nav-item")
-                                .eq(0)
-                                .should("contain", "Home");
-                            cy.get(".nav-item")
-                                .eq(1)
-                                .should("contain", "Register");
-                            cy.get(".nav-item")
-                                .eq(2)
-                                .should("contain", "Login");
-                            cy.get(".h4User")
-                                .eq(0)
-                                .should("contain", "Welcome, User.");
-                        });
-                });
-        });
-    });
 
     describe("Body of Page", () => {
         it("Shows a header", () => {
@@ -58,24 +24,6 @@ describe("Register page", () => {
             cy.get("input[data-test=password]").type("123456");
             cy.get("button[type=submit]").click();
             });
-        });
-    });
-
-    describe("Footer", () => {
-        it("Shows the footers elements", () => {
-            cy.get(".footer")
-                .eq(0)
-                .within(() => {
-                    cy.get(".Ft")
-                        .eq(0)
-                        .should("contain", "2019 Kevin O'Keeffe");
-                    cy.get(".Ft")
-                        .eq(1)
-                        .should("contain", "Private");
-                    cy.get(".Ft")
-                        .eq(2)
-                        .should("contain", "Terms of Service");
-                });
         });
     });
 });
