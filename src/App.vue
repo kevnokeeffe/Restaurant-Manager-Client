8dda592f500dc9aec66fba74c6ceb6261336016a
Kevin O'Keeffe <kevokeeffe@gmail.com>
2019-12-13 00:56:28 +0000

    Added auto logout function, 5 minute grace

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,52 @@
     <div id="app-container">
       <router-view />
     </div>
-    <Footer></Footer>
+    
+    <alert ></alert>
+    <Footer><b-alert :message=mess_alr v-if="alerter"></b-alert></Footer>
   </div>
 </template>
 
 <script>
 import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
+import * as auth from "./services/AuthService";
 export default {
   name: "app",
   components: { Footer, NavBar },
   beforeCreate: function() {
     this.$store.dispatch("authenticate");
+  },
+  created() {
+    this.mess_alr = "create";
+    this.alerter = false;
+  },
+  mounted: function() {
+    this.mess_alr = "mounted";
+    this.inactivityTime();
+    this.alerter = false;
+  },
+  methods: {
+    inactivityTime() {
+      document.onload = this.resetTimer;
+      document.onmousemove = this.resetTimer;
+      document.ontouchstart = this.resetTimer;
+      document.onclick = this.resetTimerdocument.onscroll = this.resetTimer;
+      document.onkeypress = this.resetTimer;
+    },
+    logout() {
+      auth.logout();
+    },
+    resetTimer() {
+      clearTimeout(this.interval);
+      this.interval = setTimeout(this.countDown, 300000);
+    },
+    countDown() {
+      clearTimeout(this.interval);
+      this.mess_alr = "auto time out";
+      this.alerter = true;
+      this.logout();
+    }
   }
 };
 </script>
